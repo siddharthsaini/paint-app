60a232259ed3e4c11b400b078e71f5c6c41f80ca
Siddharth Saini <thesiddharthsaini@gmail.com>
2020-05-20 18:33:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'draw_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(DrawApp());
+
+class DrawApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Draw(),
+    );
+  }
+}
